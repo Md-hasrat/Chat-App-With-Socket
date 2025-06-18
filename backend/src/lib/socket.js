286eabe2f43a5068679747b69c064991ee00aaac
Mohammad Hasrat <hasrat233222@gmail.com>
2025-06-18 18:41:41 +0530

    updation in socket file

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,4 +1,3 @@
-
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
@@ -12,26 +11,26 @@ const io = new Server(server, {
   },
 });
 
-// used to get the socket id of the receiver --> Helper function for real time chat 
+const userSocketMap = {}; // { userId: socketId }
+
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
-
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
-  const userId = socket.handshake.query.userId;
+  // Safely extract userId from query string
+  const urlParams = new URLSearchParams(socket.handshake.url?.split("?")[1]);
+  const userId = urlParams.get("userId");
+
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
+    if (userId) delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
